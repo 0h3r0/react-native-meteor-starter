ef3fdaa5b8a21535aed5e4b4bdba241b6ffbaf9e
Ben Jones <benjaminpeterjonesesquire@gmail.com>
2016-02-18 10:57:41 +0100

    Add environment module

--- Starter/app/environment.js
+++ Starter/app/environment.js
@@ -3,16 +3,9 @@ import {Platform} from 'react-native';
 getEnvironmentName = () => {
   // Logic for figuring out platform
 
-  /*
-  if (foo) {
-    return 'production';
-  },
-  if (bar) {
-    return 'stage';
-  }
-  */
-
   return 'dev';
+  // return 'stage';
+  // return 'production';
 }
 
 Environments = {
@@ -29,10 +22,26 @@ Environments = {
     }
   },
   stage: {
-
+    ddpOptions: {
+      // host : "stage.app.com",
+      // port : 80,
+      // ssl  : false,
+      // autoReconnect : true,
+      // autoReconnectTimer : 500,
+      // maintainCollections : true,
+      // ddpVersion : '1'
+    }
   },
   production: {
-
+    ddpOptions: {
+      // host : "app.com",
+      // port : 443,
+      // ssl  : true,
+      // autoReconnect : true,
+      // autoReconnectTimer : 500,
+      // maintainCollections : true,
+      // ddpVersion : '1'
+    }
   }
 }
 
